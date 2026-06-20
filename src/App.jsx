3edbe32301c8ecd5ecf0da46bb424b34b11f4727
Harsh Solanki <harshsolanki5575@gmai.com>
2026-06-20 12:18:16 +0530

    megaBlog first Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+ 
+  return (
+    <>
+    <h1 className=''>A Blog App With AppWrite</h1>
+    </>
+  )
+}
+
+export default App
