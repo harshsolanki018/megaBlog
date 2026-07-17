951ebe5233ef9d9387bff5b4853c3404c7a688e9
Harsh Solanki <harshsolanki5575@gmai.com>
2026-07-17 14:25:51 +0530

    Refactor user data handling in App and PostForm components for improved clarity and functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -17,7 +17,7 @@ const Input =  forwardRef(function Input({
             </label>
             }
             <input 
-            type= "text" 
+            type= {type} 
             className= {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             ref= {ref}
             {...props}
@@ -27,3 +27,5 @@ const Input =  forwardRef(function Input({
     )
 }) 
 export default Input
+
+
--- src/components/postForm/PostForm.jsx
+++ src/components/postForm/PostForm.jsx
@@ -15,11 +15,14 @@ function PostForm({post}) {
         },
     }) 
     const navigate = useNavigate()
-    const userData = useSelector(state => state.auth.userData)
+    const userData = useSelector((state) => state.auth.userData)
 
     const submit = async(data) => {
+
         if (post) {
-            const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null;
+            const file = data.image[0] 
+            ? await appwriteService.uploadFile(data.image[0]) 
+            : null;
 
             if (file) {
                 appwriteService.deleteFile(post.featuredImage)
@@ -28,11 +31,12 @@ function PostForm({post}) {
             const dbPost = await appwriteService.updatePost(post.$id,
                 {...data,
                  featuredImage: file ? file.$id : undefined ,  
+                })
 
                 if (dbPost) {
                 navigate(`/post/${dbPost.$id}`)
                 }
-            })
+          
             
         }else {
             const file = await appwriteService.uploadFile(data.image[0])
